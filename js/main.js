0ba90568a084a763719a1d145854769cb2c5c25c
mrjoseph <trev_jos@hotmail.com>
2012-10-18 09:15:26 +0100

    added first html markup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+var accordion = accordion || {};
+accordion.vars = accordion.vars || {};
+accordion.app = accordion.app || {};
+
+accordion.vars = {
+	message : 'hello world',
+	message2 : 'how are you doing',
+}
+
+	accordion.app = {
+		init: function(){
+			var message = accordion.vars.message;
+			var message2 = accordion.vars.message2;
+			console.log(message +' '+message2);
+		},
+
+	};
+
+$(document).ready(function(){
+	accordion.app.init();
+});
