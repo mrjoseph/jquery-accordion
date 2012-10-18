24a58a22be9f793b6a7859a519ccc0e1699c8fa4
mrjoseph <trev_jos@hotmail.com>
2012-10-18 12:31:49 +0100

    got the jquery to open and close

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
 
+/* 
+	1) 	Close all the content containers apart from the default open container.
+	2) 	On click of the toggle button close any containers that are open and at 
+		the same time open the content container. 
+	3)	Change the sprite arrow to uillustrate that the container is open.
+	4)	
+*/
+
 var accordion = accordion || {};
 accordion.vars = accordion.vars || {};
 accordion.app = accordion.app || {};
 
 accordion.vars = {
-	test : 'hello world',
+
 }
 
 	accordion.app = {
+
+		openTab: function(open){
+			console.log('open')
+			$(open).animate({
+				height: 100
+			},500);
+		},
+		closeTab: function(){
+			console.log('Close all tabs')
+			$('.accordion-container').animate({
+				height: 0
+			},500);
+		},
 		init: function(){
-			var message = accordion.vars.message;
-			var message2 = accordion.vars.message2;
-			console.log(test);
+			$('.toggle-btn').click(function(){
+				//close any open tabs if they have a class of open
+				var open = $(this).parent().find('.accordion-container');
+				accordion.app.closeTab();
+				accordion.app.openTab(open);
+			});	
 		},
 
 	};
